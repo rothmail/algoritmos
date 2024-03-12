dbcd495d72ec91d7392347976f84ee43b97885f0
Amanda Rothmann <162368529+rothmail@users.noreply.github.com>
2024-03-12 17:19:35 -0300

    Add files via upload

---
+++ Operadores/resolucao.js
@@ -0,0 +1,35 @@
+/*
+Exercício de Adição: Declare duas variáveis, a e b, atribua valores a elas e então crie uma terceira variável, soma, que armazena a soma de a e b. Por fim, exiba o valor da soma.
+
+Exercício de Média: Declare três variáveis, nota1, nota2 e nota3, atribua valores a elas representando notas de um estudante, e então calcule a média das três notas. Armazene o resultado em uma variável chamada media e exiba o valor da média.
+
+Exercício de Conversão de Temperatura: Declare uma variável para armazenar uma temperatura em Celsius. Em seguida, crie uma segunda variável para armazenar a temperatura convertida para Fahrenheit. Utilize a fórmula de conversão (F = C * 9/5 + 32).
+
+Exercício de Área de um Retângulo: Declare duas variáveis, base e altura, e atribua valores a elas representando as dimensões de um retângulo. Em seguida, calcule a área do retângulo e armazene o resultado em uma variável chamada area. Exiba o valor da área.
+
+Exercício de Concatenação de Strings: Declare duas variáveis, nome e sobrenome, e atribua valores a elas. Em seguida, crie uma terceira variável chamada nomeCompleto que armazena a concatenação do nome e sobrenome. Exiba o valor de nomeCompleto.
+
+Exercício de Troca de Tipos: Crie duas variáveis com os valores "50" e "25" (não esqueça das aspas!). Agora, tente fazer a soma com as duas. O que acontece? O que fazer para que a soma resulte em 75?
+*/
+
+let numero1 = 60;
+let numero2 = 35;
+const soma = numero1 + numero2;
+
+console.log(soma);
+
+const nota1 = 10;
+const nota2 = 9;
+const nota3 = 8;
+const numero3 = 3;
+
+const soma1 = nota1 + nota2 + nota3;
+const divisao = soma1 / numero3;
+
+console.log(soma1);
+console.log(divisao);
+
+
+const C = 30;
+const F = C * 9/5 + 32;
+console.log(F);
